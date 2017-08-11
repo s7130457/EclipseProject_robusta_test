e64a41f96cfa687824bb20d4256cd9a807c916c6
s7130457 <s7130457@gmail.com>
2017-08-11 16:19:46 +0800

    first

---
+++ src/pro/home.java
@@ -0,0 +1,17 @@
+package pro;
+
+public class home {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		hello();
+		
+	}
+
+	private static void hello() {
+		// TODO Auto-generated method stub
+		
+	}
+	
+
+}
---
+++ src/pro/method.java
@@ -0,0 +1,11 @@
+package pro;
+
+public class method {
+	public void aaa() {
+		try {
+			
+		} catch( Exception e) {
+			
+		}
+	}
+}
